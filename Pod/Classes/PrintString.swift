200238fed247fcffcd7d53a7afd967b41ea827b8
Preeti Nehte <preeti.nehte@globant.com>
2016-03-03 13:16:18 +0530

    add printstring

---
+++ Pod/Classes/PrintString.swift
@@ -0,0 +1,19 @@
+//
+//  PrintString.swift
+//  Pods
+//
+//  Created by Preeti Nehte on 03/03/16.
+//
+//
+
+import UIKit
+
+public class PrintString: NSObject {
+    
+    public func printString(){
+        
+        let s:String = ""
+        print(s)
+    }
+    
+}
